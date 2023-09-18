e62998c35c6a9a0837fbc02b100c9bde68b8217c
Efecan Havus <Ehavus@gmail.com>
2023-09-18 16:33:19 -0200

    background image changes depending on weather

--- dom.js
+++ dom.js
@@ -19,7 +19,11 @@ function getLocationInput(){
     return locationInput.value
 }
 
-function changeBackground(){
+function changeBackground(image){
+
+    let body = document.querySelector('body')
+
+    body.style.backgroundImage = `url(${image})`
 
 }
 
@@ -51,7 +55,7 @@ function displayData(data){
             condition: { text, icon},
             temp_c, temp_f, feelslike_c, feelslike_f, humidity, wind_kph, precip_mm,    
         },
-        location: { name, region, country} 
+        location: { name, region, country, localtime} 
     } = data
 
     weather.innerText = text
@@ -64,7 +68,29 @@ function displayData(data){
     rain.innerText =' ' + precip_mm + " mm of rain"
     weatherIcon.src = icon
 
-    changeBackground()
+    let backgroundTime = localtime.slice(10,13)
+    backgroundTime = parseInt(backgroundTime,10)
+
+    if(text == 'Partly cloudy'){
+        changeBackground('partly-cloudy.jpg')
+
+    } else if(text == 'Sunny'){
+        changeBackground('clear-sky.jpg')
+
+    } else if(text == 'Overcast' || text == 'Cloudy'){
+        changeBackground('overcast.jpg')
+ 
+    } else if(text.includes('rain')){
+        changeBackground('rain.jpg')
+    } else {
+
+        if((backgroundTime >= 20 && backgroundTime <= 24) || (backgroundTime >= 0 && backgroundTime <= 6)){
+            changeBackground('night.jpg')
+
+        }
+        
+       
+    }
 
 
 
